6d39e8f31bd39ad68010a8e6f82a594de762de95
dacaiguoguo <dacaiguoguo@163.com>
2012-10-24 17:39:23 +0800

    addSygView

---
+++ gitEGo/gitEGo/SYGView.h
@@ -0,0 +1,13 @@
+//
+//  SYGView.h
+//  gitEGo
+//
+//  Created by YangBin on 12-10-24.
+//  Copyright (c) 2012年 dacaiguoguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SYGView : UIView
+
+@end
---
+++ gitEGo/gitEGo/SYGView.m
@@ -0,0 +1,31 @@
+//
+//  SYGView.m
+//  gitEGo
+//
+//  Created by YangBin on 12-10-24.
+//  Copyright (c) 2012年 dacaiguoguo. All rights reserved.
+//
+
+#import "SYGView.h"
+
+@implementation SYGView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
